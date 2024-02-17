a015d63104db2d23178824ab62897e45382da71e
uberflop <matthewwjackson1@googlemail.com>
2024-02-17 19:40:49 +0000

    Update install_portainer.sh

--- install_portainer.sh
+++ install_portainer.sh
@@ -1,3 +1,4 @@
 mkdir -p ~/docker/portainer/portainer_data
 cd ~/docker/portainer
-cp ./docker/portainer/docker_compose_portainer_ce.yml ./docker/portainer/docker-compose.yml
+cp ~/docker/portainer/docker_compose_portainer_ce.yml ~/docker/portainer/docker-compose.yml
+docker-compose up -d
